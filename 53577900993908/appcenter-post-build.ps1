f5a730f64b41e1f3f393ed6b814e13d70e095e54
Tony DeVere <tdevere@microsoft.com>
2022-08-18 15:28:04 -0500

    copied ps1 again

--- 53577900993908/appcenter-post-build.ps1
+++ 53577900993908/appcenter-post-build.ps1
@@ -13,22 +13,31 @@ if (null -ne $token)
 
     Write-Information "Seraching for all Project and Solutions in Generated Test project"
 
-    $csproj_files = Get-ChildItem /Users/runner/work/1/a/GeneratedTest -Filter *.csproj, *.sln
+    $csproj_files = Get-ChildItem /Users/runner/work/1/a/GeneratedTest -Filter AppCenter.UITest.Android.csproj
 
+    Write-Information "List Prject Files"
     $csproj_files
 
     foreach ($file in $csproj_files)
     {
-        Get-Content -Path $file.FullName
+        $f = Get-Content -Path $file.FullName -as [Collections.ArrayList]
+        Write-Information "Original Project File Content"
+        $f
+        $f.Insert(15,'    <AndroidUseSharedRuntime>False</AndroidUseSharedRuntime>') # Line 3 starting from 0
+        $f | Set-Content file.txt
+        Write-Information "Updated Project File Content"
+        $f
+
+        
     }
 
-    #nuget restore -NonInteractive /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android.sln
+    nuget restore -NonInteractive /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android.sln
 
-    #xbuild /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android.sln /p:Configuration=Release
+    xbuild /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android.sln /p:Configuration=Release
 
-    #appcenter test prepare uitest --artifacts-dir /Users/runner/work/1/a/Artifacts --app-path /Users/runner/work/1/a/build/com.companyname.x_53577900993908.apk --build-dir /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/bin/Release --debug --quiet
+    appcenter test prepare uitest --artifacts-dir /Users/runner/work/1/a/Artifacts --app-path /Users/runner/work/1/a/build/com.companyname.x_53577900993908.apk --build-dir /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/bin/Release --debug --quiet
 
-    #appcenter test run manifest --manifest-path /Users/runner/work/1/a/Artifacts/manifest.json --app-path /Users/runner/work/1/a/build/com.companyname.x_53577900993908.apk --app AppCenterSupportDocs/ManualTestOnDevice --devices any_top_1_device --test-series launch-tests --locale en_US -p msft/test-run-origin=Build/Launch --debug --quiet --token $AppCenterTokenForTest
+    appcenter test run manifest --manifest-path /Users/runner/work/1/a/Artifacts/manifest.json --app-path /Users/runner/work/1/a/build/com.companyname.x_53577900993908.apk --app AppCenterSupportDocs/ManualTestOnDevice --devices any_top_1_device --test-series launch-tests --locale en_US -p msft/test-run-origin=Build/Launch --debug --quiet --token $AppCenterTokenForTest
 
 }
 else
